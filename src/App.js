c5dbc743ed1bc6f9bd7583d1085074723aa6ab4b
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2023-12-02 23:40:09 +0530

    restyle
    added background image, set add link color

--- src/App.js
+++ src/App.js
@@ -28,36 +28,39 @@ export default function App() {
 
   return (
     <div className="App">
-      <ToastContainer
-        position="top-right"
-        autoClose={1200}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+      <div className="backgroundImage"></div>
+      <div className="frontContent">
+        <ToastContainer
+          position="top-right"
+          autoClose={1200}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
 
-      {error && toast.error({ error })}
+        {error && toast.error({ error })}
 
-      <NavBar />
+        <NavBar />
 
-      <div className="allPages">
-        <Routes>
-          <Route path="/" element={<StudentView />} />
-          <Route path="/studentDetails/:id" element={<StudentDetails />} />
-          <Route path="/studentForm/:mode/:id" element={<StudentForm />} />
-          <Route path="/classList" element={<ClassList />} />
-          <Route path="/classView/:className" element={<ClassView />} />
-          <Route path="/teacherList" element={<Teacher />} />
-          <Route path="/teacherDetails/:id" element={<TeacherDetails />} />
-          <Route path="/addTeacher/:mode/:id" element={<addTeacher />} />
-          <Route path="/teacherForm/:mode/:id" element={<TeacherForm />} />
-          <Route path="/school" element={<School />} />
-        </Routes>
+        <div className="allPages">
+          <Routes>
+            <Route path="/" element={<StudentView />} />
+            <Route path="/studentDetails/:id" element={<StudentDetails />} />
+            <Route path="/studentForm/:mode/:id" element={<StudentForm />} />
+            <Route path="/classList" element={<ClassList />} />
+            <Route path="/classView/:className" element={<ClassView />} />
+            <Route path="/teacherList" element={<Teacher />} />
+            <Route path="/teacherDetails/:id" element={<TeacherDetails />} />
+            <Route path="/addTeacher/:mode/:id" element={<addTeacher />} />
+            <Route path="/teacherForm/:mode/:id" element={<TeacherForm />} />
+            <Route path="/school" element={<School />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
